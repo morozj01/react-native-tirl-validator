998f6dc46dee7c5d85e78afe591c3bc21572ef49
Justin Moroz <morozj01@nyu.edu>
2022-12-13 08:57:59 -0500

    docs: add basic usage to README and complete example (#6)

--- README.md
+++ README.md
@@ -5,17 +5,77 @@ Utilities to enable verifying TrustlessIRL physical labels. Built for React Nati
 ## Installation
 
 ```sh
-npm install tirl-validator
+npm install --save tirl-validator
 ```
+
+## About
+
+This library facilitates easy integration with the [TrustlessIRL](https://tirl.xyz) protocol by enabling mobile applications to perform camera based scans of TIRL labels.
+
+Read more about the TrustlessIRL protocol [here.](https://docs.tirl.xyz)
+
+Successful scans must capture images of the label with the camera tilted slightly to the left, and slightly to the right - as TIRL labels are 3-Dimensional.
 
 ## Usage
 
-```js
-import { multiply } from 'tirl-validator';
+The TirlValidator class exposes a single function:
+#### validate(imagePath): Promise<scanResult>
 
-// ...
+Read the image at `imagePath` and return result if scan completed, or further scan requirements if not.
 
-const result = await multiply(3, 7);
+**Params**
+- imagePath `string` - should contain an image of a valid TIRL label
+
+**Returns**
+```ts
+{
+  labelId: string; // ID of TIRL label being scanned
+  scanDone: boolean; // Returns true when scanning is complete and no more image capture is necessary
+  valid: boolean; // Notifies whether scanned label is authentic. Field set only when scanDone is true
+  scanLeft: boolean; // Scan completed from the left side
+  scanRight: boolean; // Scan completed from the right side
+  error: Object; // Any errors will be returned here
+}
+```
+
+### Example
+
+```ts
+import { TirlValidator } from 'tirl-validator';
+
+const path = 'local/path/to/image/file';
+
+const tirlValidator = new TirlValidator('https://tirl/ceramic/node');
+
+const result = await tirlValidator.validate(path);
+
+if (result.scanDone) {
+  const { valid } = result;
+  // Handle success case
+}
+
+if (result.error) {
+  const { message, stack } = result.error;
+  // Handle error
+}
+
+const { scanLeft, scanRight } = result;
+// Continue scanning until scanDone === true
+```
+
+A full example can be found [here](https://github.com/ZKLadder/tirl-validator/blob/main/example/src/App.tsx) which utilizes [vision-camera](https://github.com/mrousavy/react-native-vision-camera) and [react-native-fs](https://github.com/itinance/react-native-fs)
+
+### Running the example
+
+```sh
+npm run bootstrap
+cd example
+
+#IOS
+npm run ios
+
+#Android
+npm run android
 ```
 
 ## Contributing
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,29 +1,184 @@
-import * as React from 'react';
+/*eslint-disable react-hooks/exhaustive-deps*/
+/*eslint-disable react-native/no-inline-styles*/
+import React, { useEffect, useRef } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Button,
+  Platform,
+} from 'react-native';
 
-import { StyleSheet, View, Text } from 'react-native';
-//import { multiply } from 'tirl-validator';
+import { TirlValidator } from 'tirl-validator';
 
-//@TODO Create full example
+import {
+  Camera,
+  CameraPermissionStatus,
+  CameraDevice,
+} from 'react-native-vision-camera';
 
-export default function App() {
-  //const [result, setResult] = React.useState<number | undefined>();
+import fs from 'react-native-fs';
+
+const App = () => {
+  const [cameraPermission, setCameraPermission] = React.useState(
+    'not-determined' as CameraPermissionStatus
+  );
+
+  const [cameraDevice, setCameraDevice] = React.useState<
+    CameraDevice | undefined
+  >(undefined);
+
+  const [scanningActive, setScanningActive] = React.useState(false);
+
+  const [validatorResponse, setValidatorResponse] = React.useState<
+    | {
+        labelId?: string;
+        scanDone?: string;
+        scanRight?: string;
+        scanLeft?: string;
+        valid?: boolean;
+      }
+    | undefined
+  >(undefined);
+
+  const intervalRef = useRef<NodeJS.Timer | undefined>(undefined);
+
+  const ref = useRef<Camera>(null);
+
+  const { current: tirlValidator } = useRef(
+    //@TODO Replace API URL with TIRL community node
+    new TirlValidator('http://10.0.2.2:7007')
+  );
+
+  //Check camera permissions on load
+  useEffect(() => {
+    async function checkCameraPermissions() {
+      const permission = await Camera.getCameraPermissionStatus();
+      setCameraPermission(permission);
+    }
+
+    checkCameraPermissions();
+  }, []);
+
+  //Get available camera device
+  useEffect(() => {
+    async function getAvailableDevices() {
+      const devices = await Camera.getAvailableCameraDevices();
+      setCameraDevice(devices[0]);
+    }
+
+    if (cameraPermission === 'authorized') getAvailableDevices();
+  }, [cameraPermission]);
+
+  useEffect(() => {
+    async function scan() {
+      if (scanningActive) {
+        //Set interval ref to interval ID
+        intervalRef.current = setInterval(
+          async () => {
+            //Take photo
+            const photo =
+              Platform.OS === 'android'
+                ? await ref?.current?.takeSnapshot()
+                : await ref?.current?.takePhoto();
+
+            //Pass in path to photo as input for tirl-validator
+            const result = await tirlValidator.validate(photo?.path as string);
+
+            //Scan complete. End image capture && log result
+            if (result.scanDone) {
+              clearInterval(intervalRef.current);
+              intervalRef.current = undefined;
+              setScanningActive(false);
+              console.log(`The result of this scan is ${result.valid}`);
+            }
+
+            //See https://github.com/ZKLadder/tirl-validator#readme for more detail on handling common error types
+            if (result.error) {
+              console.log(result.error);
+              //Handle error
+              return;
+            }
+
+            //Set response as state for display
+            setValidatorResponse(result);
+
+            //Good practice to delete captured images on every invocation
+            await fs.unlink(photo?.path as string);
+          },
+          //Capture new image every have second
+          500
+        );
+      }
+
+      if (!scanningActive) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = undefined;
+      }
+    }
+
+    scan();
+  }, [scanningActive]);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: 10</Text>
-    </View>
-  );
-}
+    <SafeAreaView>
+      <StatusBar barStyle="dark-content" />
+      <View style={{ height: '100%' }}>
+        <View
+          style={{
+            height: '60%',
+            borderBottomWidth: 1,
+          }}
+        >
+          {cameraPermission === 'not-determined' ? <ActivityIndicator /> : null}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+          {cameraPermission === 'authorized' && cameraDevice ? (
+            <Camera
+              ref={ref}
+              style={StyleSheet.absoluteFill}
+              device={cameraDevice}
+              isActive={true}
+              photo={true}
+            />
+          ) : null}
+
+          {cameraPermission === 'denied' ||
+          cameraPermission === 'restricted' ? (
+            <Text style={{ fontSize: 14, margin: 5 }}>
+              Please manually grant camera permission in your device's settings
+              and reload app
+            </Text>
+          ) : null}
+        </View>
+
+        <ScrollView>
+          {scanningActive ? (
+            <Button
+              title="End Capture"
+              onPress={() => {
+                setScanningActive(false);
+              }}
+            />
+          ) : (
+            <Button
+              title="Start Capture"
+              onPress={() => {
+                setScanningActive(true);
+              }}
+            />
+          )}
+
+          <Text style={{ fontSize: 14, margin: 5 }}>
+            {JSON.stringify(validatorResponse, null, 4)}
+          </Text>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
